074aa23184629032233a6dc8e37579753669fa1a
Ayushman <ayushman.gigs@gmail.com>
2026-04-28 19:52:24 +0530

    change in listen and serve

--- server.go
+++ server.go
@@ -121,6 +121,6 @@ func main() {
 	router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, router))
 
 }
